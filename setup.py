f69fcf25e7fe9fd6f12641ec60b3a7f7d4d9c4e5
Michael Lewis <lewismj@waioeka.awsapps.com>
2021-11-11 11:51:00 +0000

    udpate setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,10 @@ setup(name='eta',
       url="https://github.com/lewismj/eta",
       package_dir =  {'eta': 'eta'},
       packages = ['eta'],
+      install_requires=[
+          'lark-parser=0.12.0',
+          'prompt-toolkit==3.0.20',
+          'pygments==2.10.0'
+      ],
       license="BSD License",
       )
