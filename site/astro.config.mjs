a61a1e50f2670e541172504361ab2021f41d18cf
Michael Lewis <lewismj@waioeka.com>
2026-05-09 21:15:22 +0100

    site initial checkin

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -2,7 +2,8 @@
 import { defineConfig } from "astro/config";
 
 // Set BASE_PATH env var when building for GitHub Pages project URLs
-// e.g.  BASE_PATH=/eta  npm run build
+// e.g. BASE_PATH=/eta npm run build
+// @ts-ignore
 const base = process.env.BASE_PATH ?? "";
 
 export default defineConfig({
